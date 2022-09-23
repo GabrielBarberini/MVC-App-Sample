899b6e1cd1122e071ee2137c9875cb15869bf7f1
GabrielBarberini <gabriel.barberini@sumup.com>
2022-09-23 20:54:54 -0300

    adds delete user and documment CopaUsers queries

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -6,18 +6,14 @@ using System.Data.SqlClient;
 
 namespace copatroca.Repositories {
 
-    internal class UserRepository : IUserRepository
-    {
+    internal class UserRepository : IUserRepository {
         private readonly string stringConexao = "server=labsoft.pcs.usp.br,1433;database=db_4;User=usuario_4;pwd=39431322853";
 
-        public void Create(User newUser)
-        {
-            using(SqlConnection con = new SqlConnection(stringConexao))
-            {
+        public void Create(User newUser) {
+            using(SqlConnection con = new SqlConnection(stringConexao)) {
                 string queryInsert = $"INSERT INTO CopaUsers VALUES (@Nome, @Email, @Password);";
 
-                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
-                {
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con)) {
                     cmd.Parameters.AddWithValue("@Nome", newUser.Name);
                     cmd.Parameters.AddWithValue("@Email", newUser.Email);
                     cmd.Parameters.AddWithValue("@Password", newUser.Password);
@@ -28,14 +24,11 @@ namespace copatroca.Repositories {
             }
         }
 
-        public void Update(User updateUser)
-        {
-            using (SqlConnection con = new SqlConnection(stringConexao))
-            {
+        public void Update(User updateUser) {
+            using (SqlConnection con = new SqlConnection(stringConexao)) {
                 string queryInsert = $"UPDATE CopaUsers SET Nome = @Nome, Email = @Email, Password = @Password WHERE Id = @Id";
 
-                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
-                {
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con)) {
                     cmd.Parameters.AddWithValue("@Nome", updateUser.Name);
                     cmd.Parameters.AddWithValue("@Email", updateUser.Email);
                     cmd.Parameters.AddWithValue("@Password", updateUser.Password);
@@ -45,28 +38,22 @@ namespace copatroca.Repositories {
                     cmd.ExecuteNonQuery();
                 }
             }
-
-
         }
 
-        public User Read(string userEmail)
-        {
+        public User Read(string userEmail) {
             User user = new User();
 
-            using (SqlConnection con = new SqlConnection(stringConexao))
-            {
+            using (SqlConnection con = new SqlConnection(stringConexao)) {
                 string querySelect = $"SELECT * FROM CopaUsers WHERE Email = @Email";
                 con.Open();
                 SqlDataReader rdr;
 
-                using (SqlCommand cmd = new SqlCommand(querySelect, con))
-                {
+                using (SqlCommand cmd = new SqlCommand(querySelect, con)) {
                     cmd.Parameters.AddWithValue("@Email", userEmail);
                     rdr = cmd.ExecuteReader();
 
                     if (rdr.HasRows)
                         rdr.Read();
-
                     else
                         return user;
 
@@ -74,11 +61,27 @@ namespace copatroca.Repositories {
                     user.Name = rdr[1].ToString();
                     user.Email = rdr[2].ToString();
                     user.Password = rdr[3].ToString();
-
                 }
-
             }
             return user;
+        }
+
+        public void Delete(User user) {
+            using (SqlConnection con = new SqlConnection(stringConexao)) {
+                string queryInsert = $"DELETE FROM CopaUsers WHERE id = @Id;";
+
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con)) {
+                    cmd.Parameters.AddWithValue("@Id", user.Id);
+
+                    con.Open();
+                    try {
+                        cmd.ExecuteNonQuery();
+                    } catch (Exception ex) {
+                        Console.WriteLine(ex.GetType().Name);
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
         }
     }
 }
