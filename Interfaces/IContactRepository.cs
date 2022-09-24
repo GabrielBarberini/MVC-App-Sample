2b145f347756edb5afb1c6c6237216108117ba51
albierocezar <albierocezar@gmail.com>
2022-09-23 21:06:07 -0300

    CRUD implemented IContactRepository.cs

--- Interfaces/IContactRepository.cs
+++ Interfaces/IContactRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace copatroca.Interfaces
+﻿using copatroca.Models;
+
+namespace copatroca.Interfaces
 {
-    public interface IContactRepository
+    internal interface IContactRepository
     {
+        void Create(Contact newContact);
+        void Update(Contact updateContact);
+        Contact Read(string userEmail);
+
+
     }
 }
