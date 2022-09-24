0ccec59e3b7c1919764a9b595c7d405df3e4867a
unknown <cteds_06>
2022-09-23 21:07:37 -0300

    Models Update

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -4,6 +4,11 @@ using System.Text;
  
 namespace copatroca.Models { 
     internal class Contact {  
+        public int UserId { get; set; }
         public string Info { get; set; }
+        public Contact(User user)
+        {
+            this.UserId = user.Id;
+        }
     }
 } 
