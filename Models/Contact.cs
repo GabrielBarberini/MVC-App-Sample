dc59e183a9dbede8d1e807b8d40e10df24a95981
unknown <cteds_17>
2022-09-23 21:07:22 -0300

    atualizando models

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -3,7 +3,11 @@ using System.IO;
 using System.Text;
  
 namespace copatroca.Models { 
-    internal class Contact {  
+    internal class Contact { 
+        public int UserId { get; set; }
         public string Info { get; set; }
+        public Contact(User user) {
+            this.UserId = user.Id;
+        }
     }
 } 
